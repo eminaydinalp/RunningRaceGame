fee2a8e045be90773cedff42ccd3e4e18b4909fc
eminaydinalp <muhammetemin4444@gmail.com>
2022-05-13 14:32:50 +0300

    The End

---
+++ Assets/_GAME_/Script/Controller/AmigoController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class AmigoController : MonoBehaviour
+{
+    [SerializeField] private float finishMoveSpeed;
+    private GameObject _finishPoint;
+    private SpawnControl _spawnControl;
+    private Rigidbody _rigidbody;
+    
+    void OnEnable()
+    {
+        EventManager.OnFail += ChangeMoveSpeed;
+        EventManager.OnWin += DestroyObject;
+    }
+    void OnDisable()
+    {
+        EventManager.OnFail -= ChangeMoveSpeed;
+        EventManager.OnWin -= DestroyObject;
+    }
+    void Start()
+    {
+        _finishPoint = GameObject.FindGameObjectWithTag("Finish");
+        _spawnControl = FindObjectOfType<SpawnControl>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       Move();
+
+       if (_finishPoint.transform.position.z < transform.position.z)
+       {
+           gameObject.SetActive(false);
+       }
+    }   
+
+    void Move()
+    {
+        _rigidbody.MovePosition(transform.position + Vector3.back * (Time.fixedDeltaTime * MoveSpeed.instance.amigoSpeed));
+    }
+    
+    private void ChangeMoveSpeed()
+    {
+        MoveSpeed.instance.amigoSpeed = finishMoveSpeed;
+    }
+    
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+    
+
+}
---
+++ Assets/_GAME_/Script/Controller/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float finishMoveSpeed;
+    
+    private Rigidbody _rigidbody;
+    private Camera _mainCamera;
+    
+    void OnEnable()
+    {
+        EventManager.OnFail += ChangeMoveSpeed;
+        EventManager.OnWin += DestroyObject;
+    }
+    void OnDisable()
+    {
+        EventManager.OnFail -= ChangeMoveSpeed;
+        EventManager.OnWin -= DestroyObject;
+    }
+    void Start()
+    {
+        _mainCamera = Camera.main;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       Move();
+
+       if (_mainCamera.transform.position.z > transform.position.z)
+       {
+           gameObject.SetActive(false);
+       }
+    }   
+
+    void Move()
+    {
+        _rigidbody.MovePosition(transform.position + Vector3.back * (Time.fixedDeltaTime * MoveSpeed.instance.enemySpeed));
+    }
+    
+    private void ChangeMoveSpeed()
+    {
+        MoveSpeed.instance.enemySpeed = finishMoveSpeed;
+    }
+
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+    
+
+}
